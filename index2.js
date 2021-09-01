adb8785acc01e48bead9aa896f25fce69297cc40
sergey.dusyk <sd1996sdsd@gmail.com>
2021-09-01 14:58:36 +0300

    updateBrut

--- index2.js
+++ index2.js
@@ -1,5 +1,5 @@
 function login(password) {
-    return password === 'aba';
+    return password === 'Vaqo';
 }
 
 // password: a-zA-Z
@@ -7,95 +7,77 @@ function login(password) {
 let arrEn = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
 
 
-// function brut(long = 1, arr = arrEn, tryPass = '') {
-//
-//     for (let j = 0; j< arr.length; j++){
-//         for (let i = 0; i < arr.length; i++) {
-//             let tryPassword = tryPass + arr[i]
-//             console.log(tryPassword)
-//             if (login(tryPassword) === true) {
-//                 return tryPassword
-//             }
-//             if (i === arr.length - 1) {
-//                 // console.log('ind:',i)
-//                 brut(long++ , arr = arrEn, )
-//             }
-//         }
-//     }
-//
-// }
+function brut(minLength = 1, maxLength = 4, allowedChars = arrEn) {
+    let passwordLength = minLength,
+        prevCharPosition = 0;
 
-// console.log(brut())
-
-class FailedLogin extends Error {
-
-}
-
-//
-// function tryLogin(allowedChars = arrEn, minLength = 1, maxLength = 4) {
-//     let passwordChars = [],
-//         passwordPosition = 0,
-//         positionOfAllowedChar = 0,
-//         currentPasswordLength = minLength;
-//
-//     do {
-//         // passwordChars[passwordPosition] = allowedChars[positionOfAllowedChar];
-//
-//         positionOfAllowedChar++;
-//         currentPasswordLength++;
-//
-//         for (let i = 0; i < allowedChars.length; i++) {
-//             passwordChars[passwordPosition] = allowedChars[i];
-//
-//             if (i === allowedChars.length-1 && passwordPosition < currentPasswordLength) {
-//                 passwordChars[passwordPosition] = allowedChars[i];
-//
-//                 passwordPosition++;
-//
-//             }
-//
-//             console.log(passwordChars,i)
-//
-//         }
-//         // passwordChars = []
-//         // passwordPosition++;
-//         if (currentPasswordLength > maxLength) {
-//             console.log(passwordChars.join(''));
-//             throw new FailedLogin();
-//         }
-//     } while (!login(passwordChars.join('')))
-//     const password = passwordChars.join('')
-//     return password;
-// }
-//
-// try {
-//     console.log("Successfully authorized. Password: ", tryLogin())
-// } catch (error) {
-//     if (error instanceof FailedLogin) {
-//         console.log("Failed login")
-//     }
-// }
-
-
-function brut(minLength = 1, maxLength = 3, arr = arrEn) {
-
-
-    for (let j = minLength; j <= maxLength; j++) {
-        let matrix = setMatrix(j-1);
-
-        for (let i = 0; i < arr.length; i++) {
-            matrix[j] = i
-            if (i === arr.length-1) {
-                matrix[j - 1] = i;
+    parent:
+    do {
+        let matrix = setMatrix(passwordLength),
+            password = matrixToString(matrix, allowedChars),
+            charPosition;
+        do {
+            if (login(password)) {
+                console.log('Loggined', password);
+                break parent
             }
-            console.log(matrixToString(arr, matrix))
+
+            charPosition = shouldIncrement(matrix, allowedChars.length);
+            if (charPosition === null) {
+                break;
+            }
+
+            console.log(matrix,charPosition,prevCharPosition)
+
+            password = matrixToString(matrix, allowedChars)
+            matrix = incrementMatrix(matrix, charPosition, prevCharPosition)
+            prevCharPosition = charPosition;
+
+        } while (charPosition != null);
+        if (login(password)) {
+            console.log('Loggined', password);
+            break;
         }
+        passwordLength++;
+    } while (passwordLength <= maxLength);
 
-    }
-
-
+    console.log('failed login')
 }
 
+/**
+ * @param matrix
+ * @param allowedCharsLength
+ * @returns {null|number}
+ */
+function shouldIncrement(matrix, allowedCharsLength) {
+    const keys = Object.keys(matrix);
+    for (let i = keys.length - 1; i >= 0; i--) {
+        if (matrix[i] < allowedCharsLength) {
+            return i;
+        }
+    }
+    return null;
+}
+
+function incrementMatrix(matrix, currentIndex, prevIndex) {
+    matrix[currentIndex]++;
+    if (currentIndex < prevIndex) {
+        let newMatrix = {};
+        for (let key in matrix) {
+            if (key == currentIndex) {
+                    newMatrix[key] = matrix[currentIndex];
+            } else if (key < currentIndex) {
+                newMatrix[key] = matrix[key];
+            } else {
+                newMatrix[key] = 0;
+            }
+        }
+        // console.log('new',newMatrix)
+        return newMatrix
+    } else {
+        return matrix
+    }
+}
 
 function setMatrix(length) {
     let matrix = {};
@@ -105,7 +87,7 @@ function setMatrix(length) {
     return matrix
 }
 
-function matrixToString(allowedCharacters, matrix) {
+function matrixToString(matrix, allowedCharacters) {
     let characters = [];
     for (let key in matrix) {
         characters[key] = allowedCharacters[matrix[key]]
@@ -114,5 +96,16 @@ function matrixToString(allowedCharacters, matrix) {
     return characters.join('');
 }
 
-// console.log(matrixToString(arrEn, matrix));
+
 brut()
+
+
+// const a = {
+//     0: 0, 1: 52,2:52
+//
+// }
+//
+// console.log(incrementMatrix(a, 1, 2))
+
+
+// console.log(shouldIncrement(a,arrEn.length))
